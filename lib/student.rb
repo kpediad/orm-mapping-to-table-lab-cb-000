af208af6ae1a255e2140b2144573df80c036c9d6
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-11 11:23:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,4 +25,23 @@ class Student
       DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = 'DROP TABLE students'
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = 'INSERT INTO students (name, grade) VALUES (?, ?)'
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  
+
 end
